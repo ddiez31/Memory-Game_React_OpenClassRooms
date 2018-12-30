0ca3fd9dd2d917e5a9c28a959275e07c35ea7eee
David Diez <david.diez31@free.fr>
2018-12-30 23:08:27 +0100

    memory game ok

---
+++ src/GuessCount.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './GuessCount.css'
+
+const GuessCount = ({ guesses }) => <div className="guesses">{guesses}</div>
+
+// La fonction GuessCount ici…
+
+GuessCount.propTypes = {
+  guesses: PropTypes.number.isRequired,
+}
+
+export default GuessCount
+
